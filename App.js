cf5152c332a1e881c4247c32ecb3e39dc5989c2a
aldipee <aldipeee@gmail.com>
2020-04-14 21:31:58 +0700

    add splash screen

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import React, {useEffect} from 'react';
-
 import {Provider} from 'react-redux';
+import SplashScreen from 'react-native-splash-screen';
+import {StatusBar} from 'react-native';
 import {PersistGate} from 'redux-persist/integration/react';
 import {store, persistor} from './src/redux/store';
 
 import Index from './src/index';
 
 const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  });
   console.disableYellowBox = true;
   return (
     <Provider store={store}>
